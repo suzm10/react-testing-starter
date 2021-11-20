2a0da5c7aa31aa354ced5c39484c5605bfb33f8e
Suzan M <suzanmanasreh@gmail.com>
2021-11-20 10:51:50 -0500

    Please enter a valid amount

---
+++ src/components/TransactionCreateStepTwo.test.js
@@ -0,0 +1,15 @@
+import { render, screen } from "@testing-library/react";
+import TransactionCreateStepTwo from "./TransactionCreateStepTwo";
+import userEvent from "@testing-library/user-event";
+
+test("on initial render, the pay button is disabled", async () => {
+  render(<TransactionCreateStepTwo sender={{ id: "5" }} receiver={{ id: "5" }} />);
+  expect(await screen.findByRole("button", { name: /pay/i })).toBeDisabled();
+});
+
+test("if an amount and note is enteredm the pay button becomes enabled", async () => {
+  render(<TransactionCreateStepTwo sender={{ id: "5" }} receiver={{ id: "5" }} />);
+  userEvent.type(screen.getByPlaceholderText(/amount/i), "50");
+  userEvent.type(screen.getByPlaceholderText(/add a note/i), "dinner");
+  expect(await screen.findByRole("button", { name: /pay/i })).toBeEnabled();
+});
